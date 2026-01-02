71e8d549114d23480d06ee20b94fd88ebc7334a5
han31023-sudo <han31023@gmail.com>
2026-01-02 20:48:36 +0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div style={{ backgroundColor: '#000', color: 'lime', hei>
+      <div>
+        <h1 style={{ fontSize: '7rem' }}>🔊 HOREG PROJECT</h1>
+
+        <h2 style={{ color: 'white' }}>STATUS: BERHASIL CONNE>
+      </div>
+    </div>
+  )
+}
