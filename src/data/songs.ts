5f5ac2adb690b48e86bdd219c11105be02ef1fb8
han31023-sudo <han31023@gmail.com>
2026-01-03 10:27:29 +0700

    Create songs.ts

---
+++ src/data/songs.ts
@@ -0,0 +1,12 @@
+export const songs = [
+  {
+    id: '1',
+    title: 'Serana',
+    artist: 'For Revenge',
+    // Pakai link MP3 dari internet (hosting luar)
+    audioUrl: 'https://p.scdn.co/mp3-preview/38059732f7a90f3a464c8d52362fd90f96845348?cid=774b75d544a342419406560249f39097', 
+    coverUrl: 'https://path-to-your-image.jpg',
+    album: 'Perayaan Patah Hati',
+  },
+  // Tambah lagu lainnya...
+];
