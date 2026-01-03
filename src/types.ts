3a6828e7effa9db48cf85bf97134a5234f3fcf1b
han31023-sudo <han31023@gmail.com>
2026-01-03 10:57:33 +0700

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+// src/data/songs.ts
 export interface Song {
   id: string;
   title: string;
@@ -7,3 +8,16 @@ export interface Song {
   coverUrl: string;
   isLiked?: boolean;
 }
+
+export const songs: Song[] = [
+  {
+    id: '1',
+    title: 'Serana',
+    artist: 'For Revenge',
+    album: 'Perayaan Patah Hati',
+    // LINK INI HARUS BISA DIBUKA DI BROWSER
+    audioUrl: 'https://cdn.pixabay.com/audio/2022/05/17/audio_19990a424a.mp3', 
+    coverUrl: 'https://images.unsplash.com/photo-1493225255756-d9584f8606e9?w=300&h=300&fit=crop',
+    isLiked: false
+  }
+];
